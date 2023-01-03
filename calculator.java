11b6497ceb9124e4f529bda88c3ba5b0be052172
justinp04 <justin.pan688@gmail.com>
2023-01-03 16:05:51 +0800

    Create calculator.java

---
+++ calculator.java
@@ -0,0 +1,26 @@
+/*
+PROGRAM: calculator.java
+AUTHOR: Justin Pan 
+DATE: 3/1/2023
+PURPOSE: To create a functioning calculator with a user interface.
+*/
+
+public class calculator
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    private static double add(int numOne, int numTwo)
+    {
+        double sum = numOne + numTwo;
+        return sum;
+    }
+
+    private static double subtract(int numOne, int numTwo)
+    {
+        double sum = numOne - numTwo;
+        return sum;
+    }
+}
