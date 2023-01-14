1d60e9dbbeeaebb2ba79329b17b32fd5da7fcdb1
justinp04 <justin.pan688@gmail.com>
2023-01-14 11:21:13 +0800

    Added the menu and adition functionality

--- calculator.java
+++ calculator.java
@@ -5,14 +5,56 @@ DATE: 3/1/2023
 PURPOSE: To create a functioning calculator with a user interface.
 */
 
+import java.util.*;
+
 public class calculator
 {
     public static void main(String[] args)
     {
+        boolean loop = false;
+        Scanner input = new Scanner(System.in);
+        double numOne, numTwo;
+        while(!loop)
+        {
+            System.out.println("Calculator without GUI:");
+            System.out.println("Selecton from the following: ");
+            System.out.println("1. Addition");
+            System.out.println("2. Subtraction");
+            System.out.println("3. Multiplicaiton");
+            System.out.println("4. Division");
+            System.out.println("5. Leave program");
 
+            int selection = input.nextInt();
+            input.nextLine();
+
+            switch(selection)
+            {
+                case 1:
+                    System.out.println("Number one:");
+                    numOne = input.nextDouble();
+                    input.nextLine();
+                    System.out.println("Number Two: ");
+                    numTwo = input.nextDouble();
+                    input.nextLine();
+
+                    double sum = add(numOne, numTwo);
+
+                    System.out.println(sum);
+                break;
+                case 2:
+                break;
+                case 3:
+                break;
+                case 4:
+                break;
+                case 5:
+                    loop = true;
+                break;
+            }
+        }
     }
 
-    private static double add(int numOne, int numTwo)
+    private static double add(double numOne, double numTwo)
     {
         double sum = numOne + numTwo;
         return sum;
