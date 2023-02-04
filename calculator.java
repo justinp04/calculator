c7469015022b2515f15176c649b775c04e8b052b
justinp04 <justin.pan688@gmail.com>
2023-02-04 14:26:46 +0800

    Finished the menu, adding comments

--- calculator.java
+++ calculator.java
@@ -11,11 +11,13 @@ public class calculator
 {
     public static void main(String[] args)
     {
-        boolean loop = false;
+        boolean loop = false; //Required to ensure that the user stays in the menu unless they wish to exit. 
         Scanner input = new Scanner(System.in);
         double numOne, numTwo;
+
         while(!loop)
         {
+            //Menu including 
             System.out.println("Calculator without GUI:");
             System.out.println("Selecton from the following: ");
             System.out.println("1. Addition");
@@ -31,13 +33,13 @@ public class calculator
             {
                 case 1: //Addition
                     System.out.println("Number one:");
-                    numOne = input.nextDouble();
+                    numOne = input.nextDouble(); //Retrieves the first number 
                     input.nextLine();
                     System.out.println("Number Two: ");
-                    numTwo = input.nextDouble();
+                    numTwo = input.nextDouble(); //Retrives the second number the user wishes to add to the first number
                     input.nextLine();
 
-                    double sum = add(numOne, numTwo);
+                    double sum = add(numOne, numTwo); 
 
                     System.out.println(sum);
                 break;
@@ -65,7 +67,16 @@ public class calculator
 
                     System.out.println("The product of your two numbers is: " + product);
                 break;
-                case 4:
+                case 4: //division
+                    System.out.println("Numerator:");
+                    double numerator = input.nextDouble();
+                    input.nextLine();
+                    System.out.println("Denominator: ");
+                    double denominator = input.nextDouble();
+                    input.nextLine();
+
+                    double divFinal = divide(numerator, denominator);
+
                 break;
                 case 5:
                     loop = true;
@@ -75,20 +86,26 @@ public class calculator
     }
 
     private static double add(double numOne, double numTwo)
-    {
+    {//Adds the first number and the second number, returning the sum of the two numbers
         double sum = numOne + numTwo;
         return sum;
     }
 
     private static double subtract(double numOne, double numTwo)
-    {
+    {//Subtracts the second number from the first number, returning the sum. 
         double sum = numOne - numTwo;
         return sum;
     }
 
     private static double multiply(double numOne, double numTwo)
-    {
+    {//Multiplies the first number by the second number, returning the product 
         double product = numOne*numTwo;
         return product;
+    }
+
+    private static double divide(double pNumerator, double pDenominator)
+    {//Divides the numerator by the denominator, returning the final number. 
+        double finalNum = pNumerator/pDenominator;
+        return finalNum;
     }
 }
